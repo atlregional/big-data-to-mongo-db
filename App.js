086905277151a73078d8a287f556c78f3fe89f04
John Herman <hermanjohn2@gmail.com>
2021-02-26 17:18:40 -0500

    Wrote functionality to add multiple files into proper collections on mongo. Collection name data and key names for collections are taken from CSV file.

--- App.js
+++ App.js
@@ -3,47 +3,91 @@ const es = require('event-stream');
 var mongojs = require('mongojs');
 
 const databaseName = 'ztraxhistoric';
-const collectionName = 'main';
+// const collectionName = 'main';
 
 var databaseUrl = `mongodb://localhost/${databaseName}`;
 var db = mongojs(databaseUrl);
 
+db.on('error', err => {
+	console.log(err);
+	process.exit(1);
+});
+
 // Set the Mongo DB collection into which you are inserting your data
-const myCollection = db.collection(collectionName);
+// const myCollection = db.collection(collectionName);
 
 // Get array of filenames from directory using fs.readdir
-const fileName = 'ZAsmt-Main.txt'; 
+// const fileName = 'ZAsmt-Main.txt';
 
-const filePath = `../data/ztrax/historic/GA/${fileName}`;
+// const filePath = `../data/ztrax/historic/GA/${fileName}`;
 
-var totalLines = 0;
+const getCollectionKeysFromCSV = tableName => {
+	const arr = fs.readFileSync('./manifest/ZTRAX-Assessment.csv', 'utf-8').split('\r\n');
 
+	let keys = [];
 
-// Pull header array from manifest CSV 
-const headerArray;
+	for (let i = 1; i < arr.length; i++) {
+		arr[i].split(',')[0] === tableName ? keys.push(arr[i].split(',')[1]) : null;
+	}
 
+	return keys;
+};
 
-// Loop over filenames and console.log(dataObject) for each row in each file
-fs.createReadStream(filePath)
-  .pipe(es.split('\r\n'))
-  .pipe(es.mapSync(line => {
-    const lineArray = line.split("|");
+const getCollectionNamesFromCSV = () => {
+	const arr = fs.readFileSync('./manifest/ZTRAX-Assessment.csv', 'utf-8').split('\r\n');
 
-    const dataObject = {};
+	let collections = [];
 
-    headerArray.forEach((field, i) => 
-        dataObject[field] = lineArray[i]);
-        
-    
+	for (let i = 1; i < arr.length; i++) {
+		collections.push(arr[i].split(',')[0]);
+	}
 
-    // myCollection.insert(dataObject) 
+	return [...new Set(collections)];
+};
 
-    totalLines++;
-    totalLines % 10000 === 0 ? console.log(totalLines) : null;
-  })
-  .on('end', () => {
-    console.log('Entire File Read');
-    console.log(totalLines);
-  }))
-  .on('error', err => console.log('File Read Error', err)
-  )
+const insertData = () => {
+	const files = fs.readdirSync('./data', 'utf-8');
+	const collections = getCollectionNamesFromCSV();
+
+	files.forEach(file => {
+		const fileName = file.split('.')[0];
+		let totalLines = 0;
+
+		if (!collections.includes(fileName)) {
+			console.log(`Error: ${fileName}`);
+			continue;
+		}
+
+		const keys = getCollectionKeysFromCSV(fileName);
+
+		fs.createReadStream(`./data/${file}`)
+			.pipe(es.split('\n'))
+			.pipe(
+				es
+					.mapSync(line => {
+						const lineArray = line.split('|');
+						const dataObject = {};
+						keys.forEach((field, i) => {
+							dataObject[field] = lineArray[i];
+						});
+
+						// console.log(dataObject);
+
+						db.collection(fileName).insert(dataObject);
+
+						totalLines++;
+						totalLines % 10000 === 0
+							? console.log(totalLines)
+							: null;
+					})
+					.on('end', () => {
+						console.log(`${file} Read`);
+						console.log(totalLines);
+					})
+			)
+
+			.on('error', err => console.log('File Read Error', err));
+	});
+};
+
+insertData();
