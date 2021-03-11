5ddcce8027529b16676d52bac847722848b71083
John Herman <hermanjohn2@gmail.com>
2021-03-11 15:31:44 -0500

    Implemented mongojs to insert and update. Added  async functionality of array iteration to insertData.

--- App.js
+++ App.js
@@ -4,10 +4,8 @@ const es = require('event-stream');
 const mongoose = require('mongoose');
 var mongojs = require('mongojs');
 
-
-const createModel = require('./utils/createModel');
+// const createModel = require('./utils/createModel');
 const modelConfig = require('./utils/model-association-config.json');
-// const { config } = require('process');
 
 // const databaseName = process.argv[3];
 // const manifest = process.argv[2];
@@ -15,15 +13,11 @@ const modelConfig = require('./utils/model-association-config.json');
 const databaseName = 'ztraxAssessment';
 const manifest = 'ZTRAX-Assessment';
 
-const MONGODB_URI = `${process.env.MONGODB_URI}${databaseName}` || `mongodb://localhost/${databaseName}`;
+// ${process.env.MONGODB_URI}${databaseName}`
 
-
+const MONGODB_URI = `mongodb://localhost/${databaseName}`;
 
 var db = mongojs(MONGODB_URI);
-
-// Set the Mongo DB collection into which you are inserting your data
-const myCollection = db.collection('Main');
-
 
 // mongoose.connect(MONGODB_URI, {
 // 	useNewUrlParser: true,
@@ -62,107 +56,73 @@ const getCollectionKeysFromCSV = tableName => {
 	return keys.sort((a, b) => a.sorter - b.sorter).map(item => item.key);
 };
 
+const updateParent = (obj, childId, rowId) => {
+	const pushQuery = {};
+	pushQuery[obj.table] = childId;
+
+	db.collection(obj.parent).update({ RowID: rowId }, { $push: pushQuery }, err =>
+		err ? console.log(err) : null
+	);
+};
+
 const insertData = async obj => {
-	// Adding file property to objs in config array
-  let totalLines = 0;
+	let totalLines = 0;
 
-  const keys = getCollectionKeysFromCSV(obj.table);
+	const keys = getCollectionKeysFromCSV(obj.table);
 
-  // const model = await createModel(obj.table, keys);
-  // console.log(obj, model.schema);
+	const collection = db.collection(obj.table);
 
-  fs.createReadStream(`./data/ZAsmt/${obj.file}`)
-    .pipe(es.split('\n'))
-    .pipe(es.mapSync(line => {
-        const lineArray = line.split('|');
+	fs.createReadStream(`./data/${obj.file}`)
+		.pipe(es.split('\n'))
+		.pipe(
+			es
+				.mapSync(line => {
+					totalLines++;
+					totalLines % 10000 === 0 ? console.log(totalLines) : null;
 
-        const dataObject = {};
+					const lineArray = line.split('|');
 
-        keys.forEach((field, i) => {
-          dataObject[field] = lineArray[i];
-        });
-        
-        modelConfig.forEach(table => {
-          obj.table === table.parent ? dataObject[table.table] = []
-          : null
-        })
+					const dataObject = {};
 
+					keys.forEach((field, i) => {
+						dataObject[field] = lineArray[i];
+					});
 
-        // console.log(model);
+					modelConfig.forEach(table => {
+						obj.table === table.parent ? (dataObject[table.table] = []) : null;
+					});
 
+					collection.insert(dataObject, (err, data) => {
+						err ? console.log(err) : null;
 
-        // myCollection.insert(dataObject);
-
-        // console.log(dataObject);
-
-        // model
-        
-        myCollection.insert(dataObject)
-          // .then(data => {
-
-          //   totalLines++;
-          //   totalLines % 10000 === 0 ? console.log(totalLines) : null;
-    
-          //   // if (obj.parent) {
-          //   //   const updateKey = obj.table.toLowerCase();
-
-          //   //   const pushQuery = {};
-          //   //   pushQuery[updateKey] = data[0]._id;
-
-          //   //   mongoose.model(obj.parent)
-          //   //     .findOneAndUpdate(
-          //   //       { RowID: data[0].RowID },
-          //   //       { $push: pushQuery }
-          //   //     )
-          //   //     // .then(res =>
-          //   //     // 	console.log(
-          //   //     // 		`${obj.parent}: ${res.RowID} successfully updated with ${obj.table}: ${data[0].RowID}`
-          //   //     // 	)
-          //   //     // )
-          //   //     .catch(err => console.log(err));
-          //   // }
-          //   // console.log(
-          //   // 	`${obj.table}: ${data[0].RowID} successfully added`
-          //   // );
-          // })
-          // .catch(err => {
-          //   console.log(err);
-          // });
-      })
-      .on('end', () => {
-        console.log(`${obj.file} Read`);
-        console.log(totalLines);
-      })
-      // .catch(err => console.log(err))
-    )
-    .on('error', err => console.log('File Read Error', err));
+						obj.parent ? updateParent(obj, data._id, data.RowID) : null;
+					});
+				})
+				.on('end', () => {
+					console.log(`${obj.file} Read`);
+					console.log(totalLines);
+				})
+		)
+		.on('error', err => console.log('File Read Error', err));
 };
 
 const configArr = modelConfig.map(obj => {
-  return {
-    table: obj.table,
-    parent: obj.parent,
-    file: `${obj.table}.txt`
-  };
+	return {
+		table: obj.table,
+		parent: obj.parent,
+		file: `${obj.table}.txt`
+	};
 });
 
-  insertData({
-		"table": "Main",
-		"parent": null,
-    "file": "Main.txt"
-	})
+const run = async () => {
+	try {
+		for (const obj of configArr) {
+			await insertData(obj);
+		}
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+};
 
-
-
-// const run = async () => {
-//   for (const obj of configArr) {
-//       // await console.log(obj);
-//     await insertData(obj)
-//   }
-// }
-
-// run();
-// configArr.forEach(obj => insertData(obj));
-
-
-// insertData();
+run();
